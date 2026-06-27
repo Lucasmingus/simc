e1145c093a3bb27e258b4767d94d4c4e0a8a6e7c
Christian Seestern Hyllested <crillerhylle@gmail.com>
2026-06-27 19:01:13 +0200

    small improvement

--- engine/player/unique_gear_midnight.cpp
+++ engine/player/unique_gear_midnight.cpp
@@ -3401,7 +3401,7 @@ void zathatek_breath_of_corruption( special_effect_t& effect )
 
     breath_of_corruption_t( const special_effect_t& e )
       : generic_proc_t( e, "breath_of_corruption", e.player->find_spell( 1305395 ) ),
-        stack_mod( e.driver()->effectN( 2 ).base_value() / 100.0 )
+        stack_mod( e.driver()->effectN( 2 ).percent() )
     {
       base_dd_min = base_dd_max = e.driver()->effectN( 1 ).average( e );
       base_multiplier *= role_mult( e );
